35dc6075a7dd81d60c41035f9d5ebd9693fa9425
Agustin Vazquez <agustin.vazquez@treinta.co>
2022-10-17 16:52:36 -0300

    fix prop

--- app/root.tsx
+++ app/root.tsx
@@ -28,7 +28,7 @@ export default function App() {
         <Links />
         {typeof document === "undefined" ? "__STYLES__" : null}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;700&display=swap"
           rel="stylesheet"
